a1d387528e939957005fd3be3de831cf50c05b50
Kazuhiro Sato <sato@gp1000.jp>
2024-12-13 15:40:13 +0900

    :sparkles:講座用のuseEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from 'styles/Home.module.css'
 import { Footer } from 'components/Footer'
 import { Main } from 'components/Main'
 import { Header } from 'components/Header'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 
 // const handleClick = (e, foo) => {
 //   console.log(e.target.href);
@@ -19,6 +19,16 @@ export default function Home() {
     e.preventDefault();
     alert(foo);
   }, []);
+
+  useEffect(() => {
+    console.log("マウント");
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      console.log("アンマウント");
+      document.body.style.backgroundColor = "";
+    }
+  }, [])
 
   return (
     <div className={styles.container}>
