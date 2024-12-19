f948d0626f5a0b5e8c1eed8d0dd53fccf88439d0
Kazuhiro Sato <sato@gp1000.jp>
2024-12-19 15:53:17 +0900

    :sparkles:カウンターを設置してuseStateでカウントアップする

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from 'styles/Home.module.css'
 import { Footer } from 'components/Footer'
 import { Main } from 'components/Main'
 import { Header } from 'components/Header'
-import { useCallback, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 // const handleClick = (e, foo) => {
 //   console.log(e.target.href);
@@ -12,20 +12,16 @@ import { useCallback, useEffect } from 'react'
 // }
 
 export default function Home() {
-  const foo = 1;
+  const [count, setCount] = useState(1);
 
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  const handleClick = (e) => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
 
   useEffect(() => {
-    console.log("マウント");
     document.body.style.backgroundColor = "lightblue";
-
     return () => {
-      console.log("アンマウント");
       document.body.style.backgroundColor = "";
     }
   }, [])
@@ -36,9 +32,8 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <a href="/about" onClick={handleClick}>
-        ボタン
-      </a>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
       <Footer />
     </div>
