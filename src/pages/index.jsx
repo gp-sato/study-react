b62236ddec618375505724ad5899724b109cb645
Kazuhiro Sato <sato@gp1000.jp>
2024-12-20 16:04:29 +0900

    :zap:useCallback, useEffectの第２引数に関して説明しました。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,28 +3,23 @@ import styles from 'styles/Home.module.css'
 import { Footer } from 'components/Footer'
 import { Main } from 'components/Main'
 import { Header } from 'components/Header'
-import { useEffect, useState } from 'react'
-
-// const handleClick = (e, foo) => {
-//   console.log(e.target.href);
-//   e.preventDefault();
-//   alert(foo);
-// }
+import { useCallback, useEffect, useState } from 'react'
 
 export default function Home() {
   const [count, setCount] = useState(1);
 
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-  };
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((count) => count + 1);
+    }
+  }, [count]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
     return () => {
       document.body.style.backgroundColor = "";
     }
-  }, [])
+  }, []);
 
   return (
     <div className={styles.container}>
