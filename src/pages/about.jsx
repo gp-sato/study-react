6ab1fc80970dedad5a584ccf857d5549e959de49
Kazuhiro Sato <sato@gp1000.jp>
2025-02-07 14:33:55 +0900

    ♻️リファクタリング

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,30 +4,21 @@ import { Footer } from 'components/Footer'
 import { Main } from 'components/Main'
 import { Header } from 'components/Header'
 
-export default function Home({
-  doubleCount,
-  isShow,
-  handleClick,
-  handleDisplay,
-  text,
-  array,
-  handleChange,
-  handleAdd
-}) {
-
+const About = (props) => {
   return (
     <div className={styles.container}>
       <Head>
         <title>About Page</title>
       </Head>
       <Header />
-      {isShow ? <h1>{doubleCount}</h1> : null}
-      <button onClick={handleClick}>ボタン</button>
-      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
-      <input type="text" value={text} onChange={handleChange} />
-      <button onClick={handleAdd}>追加</button>
+
+      {props.isShow ? <h1>{props.doubleCount}</h1> : null}
+      <button onClick={props.handleClick}>ボタン</button>
+      <button onClick={props.handleDisplay}>{props.isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={props.text} onChange={props.handleChange} />
+      <button onClick={props.handleAdd}>追加</button>
       <ul>
-        {array.map(item => {
+        {props.array.map(item => {
           return (<li key={item}>{item}</li>)
         })}
       </ul>
@@ -36,3 +27,5 @@ export default function Home({
     </div>
   )
 }
+
+export default About;
